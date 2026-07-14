af6f81e2946948c05b4cc7c4a703907a018e0904
Oliver ZHang <baiju.zhang@icloud.com>
2026-07-13 21:25:28 -0700

    Initial Commit

---
+++ SkillSync/SkillSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillSyncApp.swift
+//  SkillSync
+//
+//  Created by Oliver Zhang on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct SkillSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
